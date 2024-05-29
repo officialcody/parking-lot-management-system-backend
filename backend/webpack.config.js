6b52326470f52b69d2a946a350e4ee232d1b4ecb
Sparsh Goel <sparshgoelt26@gmail.com>
2024-05-29 13:17:44 +0530

    Webpack build

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -2,7 +2,21 @@ import path from "path";
 
 export default {
   mode: "production",
-  entry: "./server.js",
+  entry: "./index.js",
+  resolve: {
+    fallback: {
+      aws4: false,
+      "mongodb-client-encryption": false,
+      snappy: false,
+      "gcp-metadata": false,
+      "@aws-sdk/credential-providers": false,
+      "@mongodb-js/zstd": false,
+      kerberos: false,
+    },
+  },
+  externals: {
+    express: "express",
+  },
   output: {
     path: path.join(process.cwd(), "dist"),
     publicPath: "/",
